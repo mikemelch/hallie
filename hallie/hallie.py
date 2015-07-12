7fe2bed636ef99122226ec2c59d840ba63c700a6
Michael L Melchione <michaelmelchione@Michaels-MacBook-Pro.local>
2015-07-11 21:18:45 -0500

    Added regex for sudo and mkdir commands

--- hallie/hallie.py
+++ hallie/hallie.py
@@ -5,9 +5,14 @@ import re
 from modules import *
 
 def parse(command):
-	if re.match("(please )*(show|give|tell|list|print) (me|my)*.* (files)", command):
-		files.printFiles()
+	#print re.match("(run.*(as|with)\s(priv.*|super.*|sudo|root|admin.*))|sudo|admin|super.*", command)
 
+	if re.match(r"(please\s)*(show|give|tell|list|print)\s(me|my)*.*\s(files)", command):
+		files.printFiles()
+	elif re.match(r"(run.*(as|with)\s(priv.*|super.*|sudo|root|admin.*))|sudo|admin|super.*", command):
+		files.sudo()
+	elif re.match(r"(create|make|start|build|construct|prepare|whip).*(directory|folder|catalogue)(\sin\s(?P<location>""?.*""?))*\s(and\s)(call(ed)?|name(ed)?)(\sit\s)?(?P<name>.*)", command):
+		files.mkdir()
 
 def main():
 	if len(sys.argv) < 2:
@@ -17,4 +22,3 @@ def main():
 	command = " ".join([str(sys.argv[word]) for word in range(1, len(sys.argv))]).lower()
 
 	parse(command)
-	
