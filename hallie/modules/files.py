2e290a64f206e17db2aebf113dca9eae67fde9b4
Michael L Melchione <michaelmelchione@Michaels-MacBook-Pro.local>
2015-07-11 21:19:26 -0500

    Added functionality for sudo

--- hallie/modules/files.py
+++ hallie/modules/files.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 import os
 import speech
+import subprocess
+from subprocess import Popen
 
 def printFiles():
 	"""ls command"""
 	speech.speak("Executing 'ls' command to show your files.\n")
 	os.system("ls")
+
+def sudo():
+	"""sudo the last command"""
+	speech.speak("Executing 'sudo !!' command to give the last command admin rights.\n")
+	subprocess.Popen(["!!"])
+
+def mkdir():
+	print
--- hallie/modules/speech.py
+++ hallie/modules/speech.py
@@ -6,4 +6,4 @@ def speak(words):
 	print prefix + words
 
 def emptyCommand():
-	print prefix + " Sorry, I don't understand!\n" + prefix + "Tell me something to do, like 'hallie show me the current files'"
+	print prefix + "Sorry, I don't understand!\n" + prefix + "Tell me something to do, like 'hallie show me the current files'"
