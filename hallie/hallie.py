3da77b8b0e8e69fd8d5bc1a44df693c1b57fe514
Michael L Melchione <michaelmelchione@Michaels-MacBook-Pro.local>
2015-07-18 12:12:51 -0500

    Temporarily removed sudo command and fixed ls command to use subprocess instead of os

--- hallie/hallie.py
+++ hallie/hallie.py
@@ -5,12 +5,10 @@ import re
 from modules import *
 
 def parse(command):
-	#print re.match("(run.*(as|with)\s(priv.*|super.*|sudo|root|admin.*))|sudo|admin|super.*", command)
-
 	if re.match(r"(please\s)*(show|give|tell|list|print)\s(me|my)*.*\s(files)", command):
 		files.printFiles()
-	elif re.match(r"(run.*(as|with)\s(priv.*|super.*|sudo|root|admin.*))|sudo|admin|super.*", command):
-		files.sudo()
+	#elif re.match(r"(run.*(as|with)\s(priv.*|super.*|sudo|root|admin.*))|sudo|admin|super.*", command):
+		#files.sudo()
 	elif re.match(r"(create|make|start|build|construct|prepare|whip).*(directory|folder|catalogue)(\sin\s(?P<location>""?.*""?))*\s(and\s)(call(ed)?|name(ed)?)(\sit\s)?(?P<name>.*)", command):
 		files.mkdir()
 
--- hallie/modules/files.py
+++ hallie/modules/files.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
-import os
 import speech
 import subprocess
-from subprocess import Popen
 
 def printFiles():
 	"""ls command"""
 	speech.speak("Executing 'ls' command to show your files.\n")
-	os.system("ls")
+	subprocess.call(["ls"])
 
 def sudo():
 	"""sudo the last command"""
 	speech.speak("Executing 'sudo !!' command to give the last command admin rights.\n")
-	subprocess.Popen(["!!"])
+	subprocess.call(['sudo', '!!'])
 
 def mkdir():
 	print
