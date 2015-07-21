e613d58a9f5f0d18c2727c757572c611012b05b5
Mike Melchione <michaelmelchione@gmail.com>
2015-07-20 20:57:24 -0500

    Fixed setup.py README access

--- setup.py
+++ setup.py
@@ -6,9 +6,7 @@ from setuptools import setup, find_packages
 import io
 import hallie
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
