702047ed228aa1ea16678894d5d32246546d5a75
Mike Melchione <michaelmelchione@gmail.com>
2015-07-22 21:06:57 -0500

    Added functionality for local and public IP addresses

---
+++ hallie/modules/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from requests import get
+from socket import gethostbyname, gethostname
+import speech
+
+def getPublicIPAddress():
+	"""gets the user's public IP address"""
+	speech.speak("Retrieving your public IP address. Please wait a moment.")
+	ip = get('http://api.ipify.org').text
+	speech.speak("Your public IP address is: " + ip)
+
+def getLocalIPAddress():
+	"""gets the user's local IP address"""
+	ip = gethostbyname(gethostname())
+	speech.speak("Your local IP address is: " + ip)
