b94c6da67ad9b5006ec5768876c4408699dbafee
Mike Melchione <michaelmelchione@gmail.com>
2015-07-25 12:30:58 -0500

    Minor changes in setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import hallie
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.md') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
