5b5b01a2a76faa5b59b71e70f25b48b887bdcc26
Mike Melchione <michaelmelchione@gmail.com>
2015-07-25 12:58:44 -0500

    Added a couple of tests

--- tests/test_hallie.py
+++ tests/test_hallie.py
@@ -11,18 +11,32 @@ Tests for `hallie` module.
 import unittest
 
 from hallie import hallie
+import sys
+from StringIO import StringIO
 
 
 class TestHallie(unittest.TestCase):
 
-    def setUp(self):
-        pass
+	def testEmptyCommand(self):
+		out = StringIO()
+		sys.stdout = out
+		hallie.main()
+		output = out.getvalue().strip()
+		assert "Sorry, I don't understand" in output
 
-    def test_something(self):
-        pass
+	def testBadCommand(self):
+		out = StringIO()
+		sys.stdout = out
+		hallie.parse("asdfjlasdhfjlasjdf")
+		output = out.getvalue().strip()
+		assert "I'm sorry, I don't understand that command" in output
 
-    def tearDown(self):
-        pass
+	def testGoodCommand(self):
+		out = StringIO()
+		sys.stdout = out
+		hallie.parse("play")
+		output = out.getvalue().strip()
+		assert "I'm sorry, I don't understand that command" not in output
 
 if __name__ == '__main__':
     unittest.main()
